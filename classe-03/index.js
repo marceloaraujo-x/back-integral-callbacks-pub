a38deaaddbcc9ef82fd53706fed3366089f445d8
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-07-01 21:49:08 -0300

    Resolução classe 03 - Modificando um array

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,7 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+const novasFrutas = frutas.map((x, i) => {
+    x = x.toLowerCase()
+    return `${i} - ${x.replace(x[0], x[0].toUpperCase())}`
+});
+console.log(novasFrutas);
