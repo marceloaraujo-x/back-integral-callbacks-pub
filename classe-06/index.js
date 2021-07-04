ace7acf84fdeb29c552d31c0489d9414aeb81105
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-07-04 01:05:11 -0300

    Resoluçao classe 06 - Temporizador de jogos

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,14 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+const tempoDeJogada = 10000 / jogadores.length;
+jogadores.reverse();
+function imprimirNome() {
+    if (jogadores.length === 0) {
+        console.log("A rodada terminou!");
+        clearInterval(intervalId);
+    } else {
+        console.log(jogadores[jogadores.length - 1]);
+        jogadores.length--;
+    }
+}
+imprimirNome();
+const intervalId = setInterval(imprimirNome, tempoDeJogada);
