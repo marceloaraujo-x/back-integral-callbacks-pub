28c6c58357977c7ee3e2e522366474774ab8c353
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-07-04 01:17:30 -0300

    Resolução classe 07 - Escrevendo Arquivos

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,5 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+
+const fs = require("fs");
+
+fs.writeFile("classe-07/meuarquivo.txt", "Estou aprendendo JavaScript na Cubos Academy", () => { });
